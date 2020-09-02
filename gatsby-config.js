7a88343f11f0cb36391716c7ecaf50a6927d70b7
Abhay Raj Singh <abhayonlyone@gmail.com>
2020-09-02 16:21:47 +0530

    some customisations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Abhay Raj Singh`,
+    description: `Personal portfolio site`,
+    author: `rathod-sahaab`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/greetings.tsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types"
+import React from "react"
+
+class Greeting extends React.Component {
+  constructor(props: any) {
+    super(props)
+  }
+  render() {
+    return <span className="hoverable-text">Hi!</span>
+  }
+}
+
+export default Greeting
