f814ff1518aaca9038a35cc7caa324cf434f6f0d
Abhay Raj Singh <abhayonlyone@gmail.com>
2020-09-10 01:02:59 +0530

    Moved contacts to welcome page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,51 +1,12 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faGithub,
-  faGitlab,
-  faLinkedinIn,
-} from "@fortawesome/free-brands-svg-icons"
-
 import "./header.scss"
-import { faAt, faEnvelope } from "@fortawesome/free-solid-svg-icons"
 const Header = () => (
   <header>
     <Link to="/" className="link">
       Scientist | Developer | Humble
     </Link>
-
-    <div className="header-contacts">
-      <a
-        href="https://linkedin.com/in/rathod-sahaab"
-        target="_blank"
-        style={{ color: "#0077B5" }}
-      >
-        <FontAwesomeIcon icon={faLinkedinIn} />
-      </a>
-      <a
-        href="https://github.com/rathod-sahaab"
-        target="_blank"
-        style={{ color: "#323131" }}
-      >
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-      <a
-        href="https://gitlab.com/rathod-sahaab"
-        target="_blank"
-        style={{ color: "#FC6D26" }}
-      >
-        <FontAwesomeIcon icon={faGitlab} />
-      </a>
-      <a
-        href="mailto:abhayofficialone@gmail.com"
-        target="_blank"
-        style={{ color: "#f55" }}
-      >
-	      <FontAwesomeIcon icon={faEnvelope} />
-      </a>
-    </div>
   </header>
 )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,14 @@ import SEO from "../components/seo"
 import Greeting from "../components/greetings"
 import HoverInfo from "../components/hoverInfo"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faGithub,
+  faGitlab,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+
 // import Image from "../components/image"
 
 import "./index.scss"
@@ -34,6 +42,36 @@ const IndexPage = () => (
             }
           />
           , I am a Developer
+        </div>
+        <div className="contacts">
+          <a
+            href="https://linkedin.com/in/rathod-sahaab"
+            target="_blank"
+            style={{ color: "#0077B5" }}
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+          <a
+            href="https://github.com/rathod-sahaab"
+            target="_blank"
+            style={{ color: "#323131" }}
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://gitlab.com/rathod-sahaab"
+            target="_blank"
+            style={{ color: "#FC6D26" }}
+          >
+            <FontAwesomeIcon icon={faGitlab} />
+          </a>
+          <a
+            href="mailto:abhayofficialone@gmail.com"
+            target="_blank"
+            style={{ color: "#f55" }}
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
         </div>
       </div>
     </div>
