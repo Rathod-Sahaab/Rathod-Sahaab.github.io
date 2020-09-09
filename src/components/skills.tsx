040e96c7687dc9a33dc44b07747f201efefad429
Abhay Raj Singh <abhayonlyone@gmail.com>
2020-09-10 00:29:54 +0530

    Added instructions to skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuid } from "uuid"
 
 import "./skills.scss"
 import { SkillArea, Skill, skillsCollection } from "./skillsCollection"
@@ -8,43 +8,54 @@ import HoverToolTip from "./hoverToolTip"
 const Skills = () => {
   return (
     <div id="skills" className="page">
-      <h2>My Skills</h2>
-      {skillsCollection.map((area: SkillArea) => {
-        return (
-          <div key={`skill_sector_${uuidv4()}`} className="skill-sector">
-            <div className="list-skill">
-              {area.skills.map((skill: Skill) => {
-                let { name, image, link, desc } = skill
-                return HoverToolTip(
-                  () => {
-                    return (
-                      <a
-                        href={link}
-                        key={`skill_link_${uuidv4()}`}
-                        target="_blank"
-                      >
-                        <img
-                          src={image}
-                          alt={name}
-                          style={{ height: "100%" }}
-                        />
-                      </a>
-                    )
-                  },
-                  () => {
-                    return (
-                      <div>
-                        <b>{name}</b> <span>{area.category}</span>
-                        <p dangerouslySetInnerHTML={{ __html: desc }}></p>
-                      </div>
-                    )
-                  }
-                )
-              })}
-            </div>
-          </div>
-        )
-      })}
+      <div className="content">
+        <div className="description">
+          <h2>Skills</h2>
+          <p>
+            These are some tangible skills I learned over the years denoted by
+            their respective logos. <b>Hover</b> long-press(touch devices) over
+            a logo to more about it, Its name and what I think about it.{" "}
+            <b>Click</b> any logo and it will take you to the website which
+            knows about it, more than me :p.
+          </p>
+        </div>
+        <div className="illustration">
+          {skillsCollection.map((area: SkillArea) => {
+            return (
+              <div className="list-skill">
+                {area.skills.map((skill: Skill) => {
+                  let { name, image, link, desc } = skill
+                  return HoverToolTip(
+                    () => {
+                      return (
+                        <a
+                          href={link}
+                          key={`skill_link_${uuid()}`}
+                          target="_blank"
+                        >
+                          <img
+                            src={image}
+                            alt={name}
+                            style={{ height: "100%" }}
+                          />
+                        </a>
+                      )
+                    },
+                    () => {
+                      return (
+                        <div>
+                          <b>{name}</b> <span>{area.category}</span>
+                          <p dangerouslySetInnerHTML={{ __html: desc }}></p>
+                        </div>
+                      )
+                    }
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
