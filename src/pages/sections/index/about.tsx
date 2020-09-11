98fa5843a17a04d3bf060ee4cde531f48c656fb5
Abhay Raj Singh <abhayonlyone@gmail.com>
2020-09-11 11:11:15 +0530

    more less added

--- src/pages/sections/index/about.tsx
+++ src/pages/sections/index/about.tsx
@@ -20,33 +20,34 @@ const About = () => {
   return (
     <section id="about" className="page">
       <div className="content">
-        <div className="description">
-          <h2>About</h2>
-          <p>
-            Hi, thanks for visiting! I am Abhay Raj Singh. I am a programmer
-            fairly experienced in <b>C++, Python</b> and <b>fullstack</b>{" "}
-            development. But, <i>programmer</i> being the keyword I can and I
-            will handle anything you throw at me if the problem is interesting
-            enough. I have successfully completed the{" "}
-            <b>Google Summer of Code</b>, program in 2020.
-          </p>
-          <p>
-            I am a fan of Open Source, and I actively contribute to the projects
-            I like and am intrested in. I learned programming in my highschool
-            and learning about open source was my second enlightenment towards
-            programming.
-          </p>
-          <p>
-            Apart from programming, I like graphic-designing, fitness, sciences
-            musinc and camping. Currently, I am a student at <b>NIT Hamirpur</b>
-            , Himachal Pradesh, Bharat(India).
-          </p>
-        </div>
         <div className="illustration">
           <Img
             style={{ borderRadius: "50%" }}
             fixed={query.image.childImageSharp.fixed}
           />
+        </div>
+        <div className="description">
+          <h2>About</h2>
+          <p>
+            Hi, thanks for visiting! I am Abhay Raj Singh. I am a programmer
+            fairly experienced with <b>C++, Python</b> and <b>full-stack</b>{" "}
+            development. But, <i>programmer</i> being the keyword I can handle
+            anything you throw at me. I have successfully completed the{" "}
+            <b>Google Summer of Code</b>, program in 2020.
+          </p>
+          <input type="checkbox" id="more-less-toggle" />
+          <label htmlFor="more-less-toggle">more...</label>
+          <p className="extra">
+            I am a fan of Open Source, and I actively contribute to the projects
+            I like and am intrested in. I learned programming in my highschool
+            and learning about open source was my second enlightenment towards
+            programming.
+          </p>
+          <p className="extra">
+            Apart from programming, I like graphic-designing, fitness, sciences
+            music and camping. Currently, I am a student at <b>NIT Hamirpur</b>,
+            Himachal Pradesh, Bharat(India).
+          </p>
         </div>
       </div>
     </section>
