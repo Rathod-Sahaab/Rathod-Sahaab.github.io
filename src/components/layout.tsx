7d87ba5411a66216aeb2a8a0b6396cc1abe42645
Abhay <abhayonlyone@gmail.com>
2020-12-18 14:05:07 +0530

    fix icon size

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,6 +11,11 @@ import PropTypes from "prop-types"
 import "./layout.scss"
 import Sidebar from "./sidebar"
 import Header from "./header"
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+
+// Fix huge icon flash: https://github.com/FortAwesome/react-fontawesome/issues/234
+config.autoAddCss = false
 
 const Layout = ({ children }) => {
   return (
